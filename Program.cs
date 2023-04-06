f53c8be651999f6d9f0771608677a162899a9fce
TC1005 <curvafans383@gmail.com>
2023-04-06 10:27:29 +0700

    membuat pilihan mahasiswa beserta isinya

--- Program.cs
+++ Program.cs
@@ -52,7 +52,102 @@ namespace AdministrasiKampus_RONI
                                         char ch = Convert.ToChar(Console.ReadLine());
                                         switch (ch)
                                         {
-                                           
+                                            case '1':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("DATA ADMINISTRASI KAMPUS\n");
+                                                    Console.WriteLine();
+                                                    pr.baca(conn);
+                                                }
+                                                break;
+                                            case '2':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("INPUT DATA MAHASISWA");
+                                                    Console.WriteLine("Id Mahasiswa : ");
+                                                    string NIM = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Nama Mahasiswa : ");
+                                                    string NmaMhs = Console.ReadLine();
+                                                    Console.WriteLine("Tempat , tanggal lahir : ");
+                                                    string Ttl = Console.ReadLine();
+                                                    Console.WriteLine("Alamat mahasiswa : ");
+                                                    string Alm = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan jenis kelamin (P/L) : ");
+                                                    string Jk = Console.ReadLine();
+                                                    try
+                                                    {
+                                                        pr.insert(NIM, NmaMhs, Ttl,Alm, Jk, conn);
+                                                    }
+                                                    catch
+                                                    {
+                                                        Console.WriteLine("\nAnda tidak memiliki " + "akses untuk menambah data");
+                                                    }
+                                                }
+                                                break;
+                                            case '6':
+                                                conn.Close();
+                                                return;
+                                            default:
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("\nInvalid option");
+                                                }
+                                                break;
+
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nCheck for the value entered.");
+                                    }
+                                }
+                            }
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tidak Dapat Mengakses Database Menggunakan User Tersebut\n");
+                    Console.ResetColor();
+                }
+            }
+        }
+        public void baca(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("Select * From HRD.Mahasiswa", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                for (int i = 0; i < r.FieldCount; i++)
+                {
+                    Console.WriteLine(r.GetValue(i));
+                }
+                Console.WriteLine();
+            }
+            r.Close();
+        }
+        public void insert(string NIM, string NmaMhs, string Almt, string jk, string notlpn,
+            SqlConnection con)
+        {
+            string str = "";
+            str = "insert into HRD. MAHASISWA (NIM, NamaMhs, AlamatMhs, Sex, PhoneMhs)"
+                + " values (@nim, @nma, @alamat, @JK, @Phn)";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("nim", NIM));
+            cmd.Parameters.Add(new SqlParameter("nma", NmaMhs));
+            cmd.Parameters.Add(new SqlParameter("alamat", Almt));
+            cmd.Parameters.Add(new SqlParameter("JK", jk));
+            cmd.Parameters.Add(new SqlParameter("Phn", notlpn));
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil Ditambahkan");
         }
     }
 }
